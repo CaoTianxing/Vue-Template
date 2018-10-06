205211b536290ed39d7d4bdfb52f77898a10894c
viper <2183298@qq.com>
2018-10-06 13:46:31 +0800

    vue中使用CKEditor4富文本编辑器

---
+++ src/App.vue
@@ -0,0 +1,21 @@
+<template>
+  <div id="video">
+video
+<textarea id="editor" rows="10" cols="80"></textarea>
+
+  </div>
+</template>
+<script>
+import CKEDITOR from "CKEDITOR"
+  export default{
+    name:'video',
+    mounted() {
+      CKEDITOR.replace("editor", {height: "300px", width: "100%", toolbar: "Full"});
+      var editor = CKEDITOR.instances.editor2;
+    },
+  }
+</script>
+
+<style>
+
+</style>
